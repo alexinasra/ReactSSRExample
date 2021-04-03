23864d90dc03edacfbc75cc9ed15f64fa1bdb651
Alexei Nassra <alexinasra@gmail.com>
2021-04-03 23:11:28 +0300

    generate source map

--- apps/webapp/webpack.config.js
+++ apps/webapp/webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   name: 'client',
   mode: 'development',
+  devtool: "eval-source-map",
   target: 'web',
   entry: {
     main: [
