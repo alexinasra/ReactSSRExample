d4c75a65526ae9c237df25723209e9d997908b3f
Alexei Nassra <alexinasra@gmail.com>
2021-04-17 02:34:53 +0300

    changed theme colors

--- apps/adminconsole/src/theme.js
+++ apps/adminconsole/src/theme.js
@@ -1,13 +1,13 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
-import green from '@material-ui/core/colors/lightGreen';
+import blue from '@material-ui/core/colors/blue';
+import amber from '@material-ui/core/colors/amber';
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     type: 'light',
-    primary: red,
-    secondary: green,
+    primary: blue,
+    secondary: amber,
   },
 });
 
