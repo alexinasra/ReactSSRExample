18498d89356140364cd8d415b53a781dab603cca
Alexei Nassra <alexinasra@gmail.com>
2021-06-05 21:42:16 +0300

    export dropI18nTranslation

--- apps/adminconsole/graphql/mutations/index.js
+++ apps/adminconsole/graphql/mutations/index.js
@@ -2,10 +2,12 @@ const createI18nNamespace = require('./createI18nNamespace');
 const dropI18nNamespace = require('./dropI18nNamespace');
 const createI18nTranslation = require('./createI18nTranslation');
 const updateI18nTranslation = require('./updateI18nTranslation');
+const dropI18nTranslation = require('./dropI18nTranslation');
 
 module.exports = {
   createI18nNamespace,
   dropI18nNamespace,
   createI18nTranslation,
   updateI18nTranslation,
+  dropI18nTranslation,
 }
