f2561f200cefca60eaf8007ae1794a030782fc54
Alexei Nassra <alexinasra@gmail.com>
2021-06-07 09:41:03 +0300

    updated ForceLogin

--- packages/ui/src/ForceSignin/ForceSignin.js
+++ packages/ui/src/ForceSignin/ForceSignin.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Link from '@material-ui/core/Link';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import AppLoading from '../AppLoading';
 import AuthReport from '../AuthReport';
 
+
+function RedirectDelay({ signinUrl }) {
+  const { t } = useTranslation('UI', { useSuspense: false });
+  const [count, setCount] = React.useState(0);
+  React.useEffect(() => {
+    if (count>=100) {
+      return window.location.replace(signinUrl);
+    }
+    setTimeout(() => setCount(count + 5), 120);
+  }, [count])
+  return (
+    <AppLoading loadingVariant="determinate" loadingValue={count}>
+      <Link href={signinUrl}>{t('RedirectDelay.message')}</Link>
+    </AppLoading>
+  )
+}
+
 export default function ForceSignin({ signinUrl, children }) {
+  const [counter, setCounter] = React.useState(0);
 
   return (
     <AuthReport>
-      {({userInRole}) => (
-        userInRole ? children : (
-          <Link href={signinUrl}>Signin</Link>
-        )
+      {({ userInRole }) => (
+        userInRole ? children : <RedirectDelay signinUrl={signinUrl} />
       )}
     </AuthReport>
   )
