ed3f98c76d221173df1923581400a2ad14913e5c
Alexei Nassra <alexinasra@gmail.com>
2021-06-22 13:17:12 +0300

    incommingLog subscription and Log type

--- apps/adminconsole/graphql/resolvers.js
+++ apps/adminconsole/graphql/resolvers.js
@@ -1,4 +1,11 @@
+const { LoggerPubSub } = require('@react-ssrex/utils').logger;
+
 module.exports = {
   Query: require('./queries'),
   Mutation: require('./mutations'),
+  Subscription: {
+    incommingLog: {
+      subscribe: () => LoggerPubSub.asyncIterator(['LOG_EVENT']),
+    }
+  },
 };
