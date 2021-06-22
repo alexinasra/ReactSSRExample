4ccafa35b8ed83c725c2c74e167ef5100d500674
Alexei Nassra <alexinasra@gmail.com>
2021-06-22 13:16:17 +0300

    updated logger utils, every log is published with LoggerPubSub

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,12 +1,14 @@
 const text = require('./text');
 const json = require('./json');
 const object = require('./object');
+const logger = require('./logger');
 const userDir = require('./user-dir');
 module.exports = {
   text,
   json,
   object,
-  userDir
+  userDir,
+  logger
 };
 
 module.exports.default = module.exports;
--- packages/utils/logger/index.js
+++ packages/utils/logger/index.js
@@ -1,24 +1,43 @@
 const winston = require('winston');
+const PubSub = require('apollo-server').PubSub
 
+const LoggerPubSub = new PubSub();
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'log/output.log' }),
-  ],
-});
+function createLogger(defaultMeta, level='info') {
+  const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta,
+    format: winston.format.json(),
+    transports: [
+      //
+      // - Write all logs with level `error` and below to `error.log`
+      // - Write all logs with level `info` and below to `combined.log`
+      //
+      new winston.transports.File({
+        filename: 'log/error.log',
+        level: 'error'
+      }),
+      new winston.transports.File({
+        filename: 'log/output.log'
+      }),
+    ],
+  });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
+  logger.stream({
+    start: -1
+  }).on('log', function(log) {
+    LoggerPubSub.publish('LOG_EVENT', {
+      incommingLog: log
+    });
+  });
+  return logger;
 }
-
-module.exports = logger;
+module.exports = {
+  LoggerPubSub,
+  createLogger
+};
