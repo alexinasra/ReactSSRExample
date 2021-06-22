836a260ae63fcdcf8ccef603cc1e06cdfca67728
Alexei Nassra <alexinasra@gmail.com>
2021-06-22 13:18:20 +0300

    log user sign-in activity

--- apps/auth/graphql/mutations/signin.js
+++ apps/auth/graphql/mutations/signin.js
@@ -1,3 +1,5 @@
+const { createLogger } = require('@react-ssrex/utils').logger;
+
 const {
   DbLoginBadPasswordError,
   DbLoginUserNotFoundError ,
@@ -6,12 +8,20 @@ const {
 } = require('@react-ssrex/database/DbError');
 
 module.exports = async function signin(root, { input: { email, password } }, { req, UsersDb, generateId }) {
+  const logger = createLogger({
+    module: 'auth',
+    service: 'graphql/mutations/signin'
+  });
   try {
+
     const { user } = await UsersDb.login(email, password);
 
     await (new Promise(function(resolve, reject) {
       req.login(user, (err)=> {
-        if(err) return reject(err);
+        if(err) {
+          return reject(err);
+        }
+        logger.info(`User(${user._id.toString()}) Signed in.`);
         resolve();
       });
     }));
@@ -21,11 +31,13 @@ module.exports = async function signin(root, { input: { email, password } }, { r
   } catch (e) {
     if (e instanceof DbLoginUserNotFoundError) {
 
+      logger.warn(`Email(${email}) not found in db`);
       return {
         error: 'UserNotFound'
       };
     } else if (e instanceof DbLoginBadPasswordError) {
 
+      logger.warn(`Email(${email}) bad password`);
       return {
         error: 'IncorrectPassword'
       };
