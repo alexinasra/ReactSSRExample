66ce36283d62fb13f21591f6d070a2f41eeca69a
Alexei Nassra <alexinasra@gmail.com>
2021-07-02 16:10:25 +0300

    using commander to handle arguments

--- core/server/src/index.js
+++ core/server/src/index.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const Server = require('./Server');
-
+const { program } = require('commander');
 
 // Applications
 const attachAuth = require('@react-ssrex/auth');
 const attachAdminConsole = require('@react-ssrex/adminconsole');
 const attachUserConsole = require('@react-ssrex/userconsole');
 const attachWebApp = require('@react-ssrex/webapp');
+const pkg = require('../package');
 
 /*
   * Global Variables
@@ -14,15 +15,35 @@ const attachWebApp = require('@react-ssrex/webapp');
 global.isServer = true;
 global.ROOT_DIR = path.resolve(__dirname, '../..');
 
+function collect(value, previous) {
+  return previous.concat([value]);
+}
 
+program.version(pkg.version);
+program.option('--disable-auth', 'disable auth');
+program.option('--disable-admin', 'disable adminconsole');
+program.option('--disable-user', 'disable userconsole');
+program.option('--disable-webapp', 'disable webapp');
+
+program.parse(process.argv);
 //Application Confituration
 (async () => {
-  const server = new Server([
-    '@react-ssrex/auth',
-    '@react-ssrex/adminconsole',
-    '@react-ssrex/userconsole',
-    '@react-ssrex/webapp',
-  ]);
+  const modules = [];
+  const options = program.opts();
+
+  if(!options.disableAuth) {
+    modules.push('@react-ssrex/auth')
+  }
+  if(!options.disableAdmin) {
+    modules.push('@react-ssrex/adminconsole')
+  }
+  if(!options.disableUser) {
+    modules.push('@react-ssrex/userconsole')
+  }
+  if(!options.disableWebapp) {
+    modules.push('@react-ssrex/webapp')
+  }
+  const server = new Server(modules);
   try {
     await server.start();
   } catch(e) {
