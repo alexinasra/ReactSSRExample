e5b2828ad664df491abe66312b944474e715086d
Alexei Nassra <alexinasra@gmail.com>
2021-07-21 11:02:48 +0300

    changed to select component

--- packages/ui/src/ThemePaletteSelect/ThemePaletteSelect.js
+++ packages/ui/src/ThemePaletteSelect/ThemePaletteSelect.js
@@ -1,50 +1,32 @@
 import React from 'react';
 
 import { gql, useMutation } from '@apollo/client';
-import { withStyles} from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
-import Fab from '@material-ui/core/Fab';
+import Select from '@material-ui/core/Select';
 import Icon from '@material-ui/core/Icon';
-
+import defaultTheme, {
+  deepOrangeTheme
+} from '../themes';
 import blue from '@material-ui/core/colors/blue';
 import deepOrange from '@material-ui/core/colors/deepOrange';
 
 
-const DefaultTheme = withStyles((theme) => ({
-  iconBG: {
+const useStyles = makeStyles(() => ({
+  defaultIcon: {
     backgroundColor: blue[500],
     borderRadius: '50%'
-  }
-}))(({ classes, onClick}) => {
-  return (
-    <MenuItem value="default" onClick={onClick('default')}>
-      <ListItemIcon>
-        <Icon className={classes.iconBG} />
-      </ListItemIcon>
-      <ListItemText>Default</ListItemText>
-    </MenuItem>
-  );
-})
-const DeepOrangeTheme = withStyles((theme) => ({
-  iconBG: {
+  },
+  deepOrangeIcon: {
     backgroundColor: deepOrange[500],
     borderRadius: '50%'
-  }
-}))(({ classes, onClick}) => {
-  return (
-    <MenuItem value="deepOrange" onClick={onClick('deepOrange')}>
-      <ListItemIcon>
-        <Icon className={classes.iconBG} />
-      </ListItemIcon>
-      <ListItemText>Deep Orange</ListItemText>
-    </MenuItem>
-  );
-})
-
+  },
+}))
 const SET_THEME_NAME = gql`
   mutation ($themeName: String!) {
     setThemeName(themeName: $themeName) {
@@ -59,7 +41,8 @@ export default function ThemePaletteSelect({
   color,
   fab
 }) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const theme = useTheme();
+  const classes = useStyles();
   const [setThemeName] = useMutation(SET_THEME_NAME, {
     update(cache, { data: { setThemeName } }) {
       cache.modify({
@@ -71,38 +54,27 @@ export default function ThemePaletteSelect({
       })
     }
   });
-  const handleOpenThemes = (e) => setAnchorEl(e.currentTarget);
-  const handleCloseThemes = () => setAnchorEl(null);
-  const selectItem = (themeName) => () => {
-    setThemeName({variables:{ themeName }})
-    handleCloseThemes();
+
+  const selectItem = (e) => {
+    const { value } = e.target;
+    setThemeName({variables:{ themeName: value }})
   }
-  const Component = fab ? Fab : IconButton;
   return (
-    <>
-      <Component
-        color={color}
-        className={className}
-        aria-controls="theme-menu"
-        aria-haspopup="true"
-        ref={anchorEl}
-        onClick={handleOpenThemes}
-      >
-        <Icon>
-          palette
-        </Icon>
-      </Component>
-      <Menu
-      variant="selectedMenu"
-        id="theme-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleCloseThemes}
-      >
-        <DefaultTheme onClick={selectItem} />
-        <DeepOrangeTheme onClick={selectItem} />
-      </Menu>
-    </>
+    <Select
+      id="theme-menu"
+      autoWidth
+      variant="outlined"
+      value={theme.palette.id}
+      onChange={selectItem}
+    >
+      <MenuItem value={defaultTheme.lightPalette.palette.id}>
+          <Icon className={classes.defaultIcon}>palette</Icon>
+          {defaultTheme.lightPalette.palette.name}
+      </MenuItem>
+      <MenuItem value={deepOrangeTheme.lightPalette.palette.id}>
+          <Icon className={classes.deepOrangeIcon}>palette</Icon>
+          {deepOrangeTheme.lightPalette.palette.name}
+      </MenuItem>
+    </Select>
   );
 }
