d598f8d81df7c67665a686a465a225844a6bc8fc
Alexei Nassra <alexinasra@gmail.com>
2021-10-02 13:16:14 +0300

    using GqlQuery

--- apps/webapp/src/pages/HomePage/HomePage.js
+++ apps/webapp/src/pages/HomePage/HomePage.js
@@ -10,7 +10,8 @@ import ListItemButton from '@mui/material/ListItemButton';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
-import { gql, useQuery } from '@apollo/client';
+import { gql } from '@apollo/client';
+import GqlQuery from '@react-ssrex/ui/build/GqlQuery';
 import VoteOnPoll from '../actions/VoteOnPoll';
 
 const GET_ALL_Q = gql`
@@ -40,59 +41,60 @@ query {
 `;
 
 export default function HomePage() {
-  const { data, loading, error } = useQuery(GET_ALL_Q);
-
-  if (loading) {
-    return 'loading';
-  }
-  if (error) {
-    return (<pre>{JSON.stringify(error, null, '*')}</pre>);
-  }
   return (
     <LayoutBasePage>
-      {
-        (data && data.allPolls && data.allPolls.length) ? (
-          <VoteOnPoll>
-            {({ vote }) => [...data.allPolls].reverse().map((p) => (
-              <Card
-                key={p.id}
-                raised={!p.myVote}
-                sx={{
-                  marginTop: (theme) => theme.spacing(3),
-                  marginBottom: (theme) => theme.spacing(3),
-                }}
-              >
-                <CardHeader title={p.subject} />
-                <CardContent>
-                  <List>
-                    {p.options.map((o) => (
-                      <ListItemButton
-                        selected={p.myVote && p.myVote.id === o.id}
-                        disabled={!!p.myVote}
-                        key={o.id}
-                        onClick={() => vote(p.id, o.id)}
-                      >
-                        <Typography>{o.text}</Typography>
-                        {p.myVote && (
-                        <Typography>
-                          {o.votersCount}
-                          /
-                          {p.votersCount}
-                        </Typography>
-                        )}
-                      </ListItemButton>
-                    ))}
-                  </List>
-                </CardContent>
-              </Card>
-            ))}
-          </VoteOnPoll>
-        ) : (
-          <Typography>
-            No polls available.
-          </Typography>
+      <GqlQuery query={GET_ALL_Q}>
+        {
+        ({ allPolls }) => (
+          <>
+            {
+            ((allPolls && allPolls.length) ? (
+              <VoteOnPoll>
+                {({ vote }) => [...allPolls].reverse().map((p) => (
+                  <Card
+                    key={p.id}
+                    raised={!p.myVote}
+                    sx={{
+                      marginTop: (theme) => theme.spacing(3),
+                      marginBottom: (theme) => theme.spacing(3),
+                    }}
+                  >
+                    <CardHeader title={p.subject} />
+                    <CardContent>
+                      <List>
+                        {p.options.map((o) => (
+                          <ListItemButton
+                            selected={p.myVote && p.myVote.id === o.id}
+                            disabled={!!p.myVote}
+                            key={o.id}
+                            onClick={() => vote(p.id, o.id)}
+                          >
+                            <Typography>{o.text}</Typography>
+                            {p.myVote && (
+                            <Typography>
+                              {o.votersCount}
+                              /
+                              {p.votersCount}
+                            </Typography>
+                            )}
+                          </ListItemButton>
+                        ))}
+                      </List>
+                    </CardContent>
+                  </Card>
+                ))}
+              </VoteOnPoll>
+            ) : (
+              <Typography>
+                No polls available.
+              </Typography>
+            ))
+          }
+          </>
         )
       }
+
+      </GqlQuery>
       <Fab
         sx={{
           position: 'fixed',
--- apps/webapp/src/pages/Poll/ViewPoll/ViewPoll.js
+++ apps/webapp/src/pages/Poll/ViewPoll/ViewPoll.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import LayoutPage from '@react-ssrex/ui/build/WebappLayout/LayoutBasePage';
+import GqlQuery from '@react-ssrex/ui/build/GqlQuery';
 
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 
-import { gql, useQuery } from '@apollo/client';
+import { gql } from '@apollo/client';
 
 import VoteOnPoll from '../../actions/VoteOnPoll';
 
@@ -36,51 +37,45 @@ query ($pollId: String!) {
 
 export default function ViewPoll() {
   const { pollId } = useParams();
-  const { data, loading, error } = useQuery(GET_POLL_Q, {
-    variables: { pollId },
-  });
-
-  if (loading) {
-    return (<p>Loading</p>);
-  }
-  if (error) {
-    return (<pre>{JSON.stringify(error, null, '*')}</pre>);
-  }
   return (
     <LayoutPage decorate>
-      <VoteOnPoll>
-        { ({ vote }) => (
-          <Box
-            sx={{
-              marginTop: (theme) => theme.spacing(3),
-              marginBottom: (theme) => theme.spacing(3),
-            }}
-          >
-            <Typography variant="title">{data.getPoll.subject}</Typography>
-            <Box>
-              <List>
-                {data.getPoll.options.map((o) => (
-                  <ListItemButton
-                    selected={data.getPoll.myVote && data.getPoll.myVote.id === o.id}
-                    disabled={!!data.getPoll.myVote}
-                    onClick={() => vote(data.getPoll.id, o.id)}
-                    key={o.id}
-                  >
-                    <Typography>{o.text}</Typography>
-                    {data.getPoll.myVote && (
-                    <Typography>
-                      {o.votersCount}
-                      /
-                      {data.getPoll.votersCount}
-                    </Typography>
-                    )}
-                  </ListItemButton>
-                ))}
-              </List>
-            </Box>
-          </Box>
+      <GqlQuery query={GET_POLL_Q} variables={{ pollId }}>
+        {({ getPoll }) => (
+          <VoteOnPoll>
+            { ({ vote }) => (
+              <Box
+                sx={{
+                  marginTop: (theme) => theme.spacing(3),
+                  marginBottom: (theme) => theme.spacing(3),
+                }}
+              >
+                <Typography variant="title">{getPoll.subject}</Typography>
+                <Box>
+                  <List>
+                    {getPoll.options.map((o) => (
+                      <ListItemButton
+                        selected={getPoll.myVote && getPoll.myVote.id === o.id}
+                        disabled={!!getPoll.myVote}
+                        onClick={() => vote(getPoll.id, o.id)}
+                        key={o.id}
+                      >
+                        <Typography>{o.text}</Typography>
+                        {getPoll.myVote && (
+                        <Typography>
+                          {o.votersCount}
+                          /
+                          {getPoll.votersCount}
+                        </Typography>
+                        )}
+                      </ListItemButton>
+                    ))}
+                  </List>
+                </Box>
+              </Box>
+            )}
+          </VoteOnPoll>
         )}
-      </VoteOnPoll>
+      </GqlQuery>
     </LayoutPage>
   );
 }
