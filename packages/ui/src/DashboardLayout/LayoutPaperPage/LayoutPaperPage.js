4e22094ef98f3224a67be2f0a41e68be7bc2e0d3
Alexei Nassra <alexinasra@gmail.com>
2021-10-07 12:55:40 +0300

    clear elevation effect

--- packages/ui/src/DashboardLayout/LayoutPaperPage/LayoutPaperPage.js
+++ packages/ui/src/DashboardLayout/LayoutPaperPage/LayoutPaperPage.js
@@ -8,30 +8,15 @@ import BasePage from '../LayoutBasePage';
 
 import { styled } from '@mui/material/styles';
 
-const PaperPageDecorator = styled(BasePage)(({ theme }) => ({
-  paddingTop: theme.spacing(6)
-}))
 export default function LayoutPaperPage({
   title,
   subheader,
   avatar = (<Avatar><Icon>bookmark_border</Icon></Avatar>),
   children,
 }) {
-  const [elevation, setElevation] = React.useState(18);
-
-  const handleMouseEnter = () => {
-    setElevation(24);
-  };
-  const handleMouseLeave = () => {
-    setElevation(12);
-  };
   return (
-    <PaperPageDecorator decorate>
-      <Card
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        elevation={elevation}
-      >
+    <BasePage decorate>
+      <Card elevation={3}>
         {title && (
           <CardHeader
             sx={{
@@ -48,6 +33,6 @@ export default function LayoutPaperPage({
           {children}
         </CardContent>
       </Card>
-    </PaperPageDecorator>
+    </BasePage>
   );
 }
