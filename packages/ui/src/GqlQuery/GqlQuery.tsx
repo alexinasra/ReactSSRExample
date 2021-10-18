49366f21383729daa8d01b38e25f669f77e3bdc3
Alexei Nassra <alexinasra@gmail.com>
2021-10-18 13:51:51 +0300

    migrate to typescript

--- packages/ui/src/GqlQuery/GqlQuery.tsx
+++ packages/ui/src/GqlQuery/GqlQuery.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, DocumentNode, ApolloQueryResult } from '@apollo/client';
 
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
+
+type gqlQueryProps = {
+  query: DocumentNode,
+  variables: any,
+  renderError: (e: Error) => JSX.Element,
+  renderLoading: () => JSX.Element,
+  children: <T, V>(data: T, refetch: (variables?: Partial<V>) => Promise<ApolloQueryResult<T>>) => React.ReactNode
+}
 
 export default function GqlQuery({
   query,
@@ -25,7 +33,7 @@ export default function GqlQuery({
       <Skeleton animation={false} />
     </Box>),
   children
-}) {
+}: gqlQueryProps) {
   const { data, loading, error, refetch } = useQuery(query, { variables });
 
   if (loading) {
@@ -34,5 +42,6 @@ export default function GqlQuery({
   if (error) {
     return renderError(error);
   }
+
   return children(data, refetch);
 }
